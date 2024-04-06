a62130e242e96a871c42801406d635a7cfc6915d
Simoni Washiya <simoni.washiya@fatec.sp.gov.br>
2024-04-06 20:31:43 -0300

    Update TesteIngresso.java

--- TAREFAREPOSICAO1EX01/REPOSICAO1EX1/src/view/TesteIngresso.java
+++ TAREFAREPOSICAO1EX01/REPOSICAO1EX1/src/view/TesteIngresso.java
@@ -3,18 +3,18 @@ package view;
 import model.Ingresso;
 import model.IngressoVip;
 
-// Classe de teste para as implementações de Ingresso e IngressoVIP
+
 public class TesteIngresso {
     public static void main(String[] args) {
-        // Criando um ingresso comum
+       
         Ingresso ingressoComum = new Ingresso("A001", 50.0f);
-        // Calculando o valor final com uma taxa de conveniência de 5%
+    
         float valorFinalComum = ingressoComum.valorFinal(5.0f);
         System.out.println("Valor final do ingresso comum: R$" + valorFinalComum);
 
-        // Criando um ingresso VIP
+    
         IngressoVip ingressoVip = new IngressoVip("V001", 100.0f, "VIP Gold");
-        // Calculando o valor final com uma taxa de conveniência de 5%
+    
         float valorFinalVIP = ingressoVip.valorFinal(5.0f);
         System.out.println("Valor final do ingresso VIP: R$" + valorFinalVIP);
     }
