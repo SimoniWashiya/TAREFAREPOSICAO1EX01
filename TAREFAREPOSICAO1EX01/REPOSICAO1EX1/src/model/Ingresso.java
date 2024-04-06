1b223506db9bdea6383696eb219c176177376001
Simoni Washiya <simoni.washiya@fatec.sp.gov.br>
2024-04-06 20:32:24 -0300

    Update Ingresso.java

--- TAREFAREPOSICAO1EX01/REPOSICAO1EX1/src/model/Ingresso.java
+++ TAREFAREPOSICAO1EX01/REPOSICAO1EX1/src/model/Ingresso.java
@@ -1,23 +1,22 @@
 package model;
 
-//Classe principal que representa um ingresso
+
 public class Ingresso {
- // Atributos privados
+
  private String codigoIdentificador;
  private float valor;
 
- // Construtor
  public Ingresso(String codigoIdentificador, float valor) {
      this.codigoIdentificador = codigoIdentificador;
      this.valor = valor;
  }
 
- // Método para calcular o valor final do ingresso com taxa de conveniência
+
  public float valorFinal(float taxaConveniencia) {
      return this.valor + taxaConveniencia;
  }
 
- // Getters e Setters
+
  public String getCodigoIdentificador() {
      return codigoIdentificador;
  }
